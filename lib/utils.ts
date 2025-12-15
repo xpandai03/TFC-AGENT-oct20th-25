ce655579b60e2ffd7077724f896fce2b7dbb2054
BhuvaneshPatil <bhuvaneshpatil80642@gmail.com>
2025-12-15 20:15:23 +0530

    updated code

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function safeJsonParse<T>(jsonString: string, defaultValue: T): T {
+  try {
+    return JSON.parse(jsonString) as T
+  } catch (error) {
+    console.error('JSON parse error:', error)
+    return defaultValue
+  }
+}
